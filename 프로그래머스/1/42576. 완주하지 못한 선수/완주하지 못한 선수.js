dbcefd3fbd877b10563f322aa3e77bc699c8ec87
Seoyoon Park <63403699+yeleepark@users.noreply.github.com>
2025-04-11 20:38:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.26 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,15 @@
+function solution(participant, completion) {
+  const map = new Map();
+
+  for (let name of participant) {
+    map.set(name, (map.get(name) ?? 0) + 1);
+  }
+
+  for (let name of completion) {
+    map.set(name, map.get(name) - 1);
+  }
+
+  for (let [name, count] of map) {
+    if (count > 0) return name;
+  }
+}
